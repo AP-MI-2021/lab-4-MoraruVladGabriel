fcb9cbbb7c9e2faf20e7ec736bbfdbfff0145014
moraru vlad <moraruvlad51@yahoo.com>
2021-10-20 15:22:51 +0300

    verificare1

---
+++ main.py
@@ -0,0 +1,10 @@
+
+if __name__ == "__main__":
+    while True:
+        opt = input("Dati optiunea: ")
+        if opt == '1':
+            pass
+        elif opt == 'x':
+            break
+        else:
+            print("Optiune gresita!Reincercati.")
